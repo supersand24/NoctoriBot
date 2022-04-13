355b4b77b180545d6395a9bf8b0547f6068511a3
Justin Sandman <supersand24@gmail.com>
2022-04-13 10:08:28 -0400

    Exported Variables Accessing to new Class "Var"

--- src/main/java/Bot/Main.java
+++ src/main/java/Bot/Main.java
@@ -6,13 +6,9 @@ import net.dv8tion.jda.api.requests.GatewayIntent;
 import net.dv8tion.jda.api.utils.MemberCachePolicy;
 
 import javax.security.auth.login.LoginException;
-import java.io.FileNotFoundException;
-import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.NoSuchFileException;
 import java.nio.file.Paths;
-import java.time.LocalDate;
-import java.util.List;
 
 public class Main {
 
@@ -52,80 +48,4 @@ public class Main {
         return "";
     }
 
-    public static int getMoney(String userID) {
-        try {
-            return Integer.parseInt(Files.readAllLines(Paths.get("variables/" + userID + ".var")).get(0));
-        } catch (NoSuchFileException e) {
-            System.out.println(userID + ".var file not found!");
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return 0;
-    }
-
-    public static LocalDate getDailyClaimed(String userID) {
-        try {
-            return LocalDate.parse(Files.readAllLines(Paths.get("variables/" + userID + ".var")).get(1));
-        } catch (NoSuchFileException e) {
-            System.out.println(userID + ".var file not found!");
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return LocalDate.now();
-    }
-
-    public static int getDailiesClaimed(String userID) {
-        try {
-            return Integer.parseInt(Files.readAllLines(Paths.get("variables/" + userID + ".var")).get(2));
-        } catch (NoSuchFileException e) {
-            System.out.println(userID + ".var file not found!");
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return 0;
-    }
-
-    public static List<String> getMembersInvited(String userID) {
-        try {
-            return List.of(Files.readAllLines(Paths.get("variables/" + userID + ".var")).get(3).split(","));
-        } catch (NoSuchFileException e) {
-            System.out.println(userID + ".var file not found!");
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return null;
-    }
-
-    public static String getInvitedByMember(String userID) {
-        try {
-            return Files.readAllLines(Paths.get("variables/" + userID + ".var")).get(4);
-        } catch (NoSuchFileException e) {
-            System.out.println(userID + ".var file not found!");
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return null;
-    }
-
-    public static LocalDate getGameClaimed(String userID) {
-        try {
-            return LocalDate.parse(Files.readAllLines(Paths.get("variables/" + userID + ".var")).get(5));
-        } catch (NoSuchFileException e) {
-            System.out.println(userID + ".var file not found!");
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return LocalDate.now();
-    }
-
-    public static List<String> getGameKeys(String userID) {
-        try {
-            return List.of(Files.readAllLines(Paths.get("variables/" + userID + ".var")).get(6).split(","));
-        } catch (NoSuchFileException e) {
-            System.out.println(userID + ".var file not found!");
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return null;
-    }
 }
