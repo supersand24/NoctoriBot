b4ab99f449d4f7e917fa7da99e65da802e7aacea
supersand24 <supersand24@gmail.com>
2022-06-09 13:04:20 -0400

    Changed New Channel to work as Stage Channel

--- src/main/java/Bot/AutoVoice.java
+++ src/main/java/Bot/AutoVoice.java
@@ -1,8 +1,10 @@
 package Bot;
 
+import net.dv8tion.jda.api.Permission;
 import net.dv8tion.jda.api.entities.*;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 public class AutoVoice {
@@ -11,12 +13,32 @@ public class AutoVoice {
     private final List<Member> channelAdmins = new ArrayList<>();
 
     public AutoVoice(Member member, VoiceChannel voiceChannel) {
-        channelAdmins.add(member);
         this.voiceChannel = voiceChannel;
+        addMemberAsChannelAdmin(member);
     }
 
     public VoiceChannel getVoiceChannel() {
         return voiceChannel;
+    }
+
+    public void addMemberAsChannelAdmin(Member member) {
+        channelAdmins.add(member);
+        Collection<Permission> allowed = new ArrayList<>();
+        allowed.add(Permission.KICK_MEMBERS);
+        allowed.add(Permission.MANAGE_CHANNEL);
+        allowed.add(Permission.MANAGE_PERMISSIONS);
+        allowed.add(Permission.CREATE_INSTANT_INVITE);
+        allowed.add(Permission.VOICE_DEAF_OTHERS);
+        allowed.add(Permission.VOICE_MUTE_OTHERS);
+        allowed.add(Permission.MESSAGE_MENTION_EVERYONE);
+        allowed.add(Permission.MESSAGE_MENTION_EVERYONE);
+        getVoiceChannel().getManager().putMemberPermissionOverride(member.getIdLong(),allowed, new ArrayList<>()).queue();
+    }
+
+    public void removeMemberAsChannelAdmin(Member member) {
+        System.out.println(channelAdmins);
+        channelAdmins.remove(member);
+        getVoiceChannel().getPermissionOverride(member).getManager().resetAllow().queue();
     }
 
 }
--- src/main/java/Bot/AutoVoiceManager.java
+++ src/main/java/Bot/AutoVoiceManager.java
@@ -4,53 +4,62 @@ import net.dv8tion.jda.api.entities.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class AutoVoiceManager {
 
     private final static Logger log = LoggerFactory.getLogger(AutoVoiceManager.class);
-
     public final static List<AutoVoice> channels = new ArrayList<>();
-    private final static long textChannelId = 964666264867967008L;
 
     public static void join(Member member, AudioChannel audioChannel) {
-        join(member,convertAudioChannel(audioChannel));
-    }
-    public static void join(Member member, VoiceChannel voiceChannel) {
-        if (voiceChannel.getName().equalsIgnoreCase("New Channel")) {
-            newChannel(member, voiceChannel);
+        if (audioChannel.getName().equalsIgnoreCase("New Channel")) {
+            newChannel(member, (StageChannel) audioChannel);
         }
     }
 
-    public static void newChannel(Member member, VoiceChannel voiceChannel) {
-        voiceChannel.createCopy().queue();
-        renameChannel(voiceChannel,"Vibing");
-        log.info("New Channel Created.");
+    public static void leave(Member member, AudioChannel audioChannel) {
+        if (!audioChannel.getName().equals("New Channel")) {
+            if (audioChannel.getType() == ChannelType.VOICE) {
+                VoiceChannel voiceChannel = (VoiceChannel) audioChannel;
+                if (voiceChannel.getParentCategory().getName().equals("Auto Voice (WIP)")) {
+                    if (voiceChannel.getMembers().size() <= 0) {
+                        channels.remove(getAutoVoice(voiceChannel));
+                        voiceChannel.delete().queue();
+                    }
+                }
+            }
+        }
+    }
+
+    public static void newChannel(Member member, StageChannel stageChannel) {
+        stageChannel.getParentCategory().createVoiceChannel("Vibing").setPosition(0).queue(voiceChannel -> {
+            log.info("New Channel Created.");
+            channels.add( new AutoVoice(member, voiceChannel) );
+            Main.getNoctori().moveVoiceMember(member,voiceChannel).queue();
+        });
     }
 
     public static void updateChannelName(AudioChannel audioChannel) {
-        updateChannelName(convertAudioChannel(audioChannel));
-    }
+        if (!audioChannel.getName().equals("New Channel")) {
+            if (audioChannel.getType() == ChannelType.VOICE) {
+                VoiceChannel voiceChannel = (VoiceChannel) audioChannel;
+                if (voiceChannel.getParentCategory().getName().equals("Auto Voice (WIP)")) {
+                    Map<String, Integer> hashMap = getVoiceChannelActivities(voiceChannel);
+                    String mostCommonKey = "Vibing";
+                    int maxValue = 1;
+                    for (Map.Entry<String, Integer> entry : hashMap.entrySet()) {
+                        if (entry.getValue() > maxValue) {
+                            mostCommonKey = entry.getKey();
+                            maxValue = entry.getValue();
+                        }
+                    }
 
-    public static void updateChannelName(VoiceChannel voiceChannel) {
-        if (voiceChannel.getParentCategory().getName().equals("Auto Voice (WIP)")) {
-            Map<String, Integer> hashMap = getVoiceChannelActivities(voiceChannel);
-            String mostCommonKey = "Vibing";
-            int maxValue = 1;
-            for (Map.Entry<String, Integer> entry : hashMap.entrySet()) {
-                if (entry.getValue() > maxValue) {
-                    mostCommonKey = entry.getKey();
-                    maxValue = entry.getValue();
+                    if (voiceChannel.getName().equals(mostCommonKey)) {
+                        log.debug("Attempted to rename " + voiceChannel.getName() + " VC to " + mostCommonKey);
+                    } else {
+                        renameChannel(voiceChannel, mostCommonKey);
+                    }
                 }
-            }
-
-            if (voiceChannel.getName().equals(mostCommonKey)) {
-                log.debug("Attempted to rename " + voiceChannel.getName() + " VC to " + mostCommonKey);
-            } else {
-                renameChannel(voiceChannel, mostCommonKey);
             }
         }
     }
@@ -66,9 +75,10 @@ public class AutoVoiceManager {
                             switch (activity.getName()) {
                                 case "Fortnite" -> {
                                     if (activity.isRich()) {
-                                        switch (activity.asRichPresence().getDetails()) {
-                                            case "Battle Royale - In Lobby" -> hashMap.merge("Fortnite: Battle Royale", 1, Integer::sum);
-                                            case "Save The World" -> hashMap.merge("Fortnite: Save the World", 1, Integer::sum);
+                                        System.out.println(activity.asRichPresence());
+                                        switch (activity.asRichPresence().getDetails().split(" ")[0]) {
+                                            case "Battle" -> hashMap.merge("Fortnite: Battle Royale", 1, Integer::sum);
+                                            case "Save" -> hashMap.merge("Fortnite: Save the World", 1, Integer::sum);
                                             default -> {
                                                 log.info("Unchecked Fortnite Rich Presence Case : " + activity.asRichPresence().getDetails());
                                                 hashMap.merge(activity.getName(), 1, Integer::sum);
@@ -91,23 +101,6 @@ public class AutoVoiceManager {
         audioChannel.getManager().setName(name).queue();
     }
 
-    public static void renameChannel(VoiceChannel voiceChannel, String name) {
-        log.info("Renamed " + voiceChannel.getName() + " VC to " + name);
-        voiceChannel.getManager().setName(name).queue();
-    }
-
-    public static void leave(Member member, AudioChannel audioChannel) {
-        leave(member,convertAudioChannel(audioChannel));
-    }
-
-    public static void leave(Member member, VoiceChannel voiceChannel) {
-        if (voiceChannel.getParentCategory().getName().equals("Auto Voice (WIP)")) {
-            if (voiceChannel.getMembers().size() <= 0) {
-                voiceChannel.delete().queue();
-            }
-        }
-    }
-
     private static AutoVoice getAutoVoice(VoiceChannel voiceChannel) {
         for (AutoVoice av : channels) {
             if (av.getVoiceChannel().getIdLong() == voiceChannel.getIdLong()) {
@@ -117,7 +110,4 @@ public class AutoVoiceManager {
         return null;
     }
 
-    private static VoiceChannel convertAudioChannel(AudioChannel audioChannel) {
-        return Main.getNoctori().getVoiceChannelById(audioChannel.getId());
-    }
 }
